f4d4777a49336730d73ff5e414f6af9407a06ad1
c-asakawa <casakawa@pdx.edu>
2016-06-28 18:38:18 -0700

    added new landing page

--- OAuth2/src/index.php
+++ OAuth2/src/index.php
@@ -3,7 +3,14 @@
  *
  * Author: Chris Asakawa
  * Date: 6/22/16
--->
+ *
+ * Authentication Flow:
+ *  1. homepage, user input username and password
+ *  2. redirect to sen.se auth page, returns code
+ *  3. redirect to sen.se token page, returns tokens
+ *  4. redirect to confirmation page
+ -->
+
 <html>
 <head>
     <meta name="description" content="">
@@ -14,75 +21,26 @@
 <body>
 <?php
 
-require('Client.php');
-require('GrantType/IGrantType.php');
-require('GrantType/AuthorizationCode.php');
-
-/* parameters */
-const CLIENT_ID     = '';
-const CLIENT_SECRET = '';
-const SCOPE         = 'devices.read';
-const REDIRECT_URI  = 'http://127.0.0.1:1234/src/index.php'; //url of this.php
-const GRANT_TYPE    = 'authorization_code';
-
-/* endpoints */
-const AUTHORIZATION_ENDPOINT = 'https://sen.se/api/v2/oauth2/authorize';
-const TOKEN_ENDPOINT         = 'https://apis.sen.se/v2/oauth2/token/';
-
-$client = new OAuth2\Client(CLIENT_ID, CLIENT_SECRET);
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-
-if (!isset($username) && !isset($password)){
-    echo <<<TAG
-        <ul>
-            <li id=logo></li>
-            <li>
-                <h4>
-                    Welcome to the CareWheels authentication page. Lets
-                    Authenticate with Sen.se, Enter the CareBank user
-                    credentials, and click the authenticate button to
-                    begin.
-                </h4>
-            </li>
-            <li>
-                <form action="index.php" method="post">
-                    Username: <input type="text" name="username" required value=$username><br>
-                    Password:&nbsp; <input type="password" name="password" required value='$password'><br>
-                    <input id="authButton" type="submit" value="authenticate">
-                </form>      
-            </li>
-        </ul>
+echo <<<TAG
+    <ul>
+        <li id=logo></li>
+        <li>
+            <h4>
+                Welcome to the CareWheels authentication page. Lets
+                Authenticate with Sen.se, click the authenticate 
+                button to begin.
+            </h4>
+        </li>
+        <li>
+            <form action="callback.php" method="post">
+                <input id="authButton" type="submit" value="authenticate">
+            </form>      
+        </li>
+    </ul>
 TAG;
-}
 
 
-/* if no auth code then retrieve the authentication code */
-else if (!isset($_GET['code']))
-{
-    $auth_url = $client->getAuthenticationUrl(
-                            AUTHORIZATION_ENDPOINT,
-                            REDIRECT_URI,
-                            array("scope" => SCOPE)
-                         );
-    header('Location: ' . $auth_url);
-    die('Redirect');
-}
-else /* use the auth code to get the access and refresh tokens */
-{
-    $params = array('code' => $_GET['code'], 'redirect_uri' => REDIRECT_URI);
-    $response = $client->getAccessToken(TOKEN_ENDPOINT, 'authorization_code', $params);
 
-    /* testing */
-    echo "access token: ";
-    print_r($response["result"]["access_token"]);
-    echo "<br><br>";
-    echo "refresh token: ";
-    print_r($response["result"]["refresh_token"]);
-
-
-}
 ?>
 </body>
 </html>
