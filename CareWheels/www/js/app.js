f063ae1176deb952887342f5d809d12d34492e75
Raven <rswartz@pdx.edu>
2016-07-03 18:57:28 -0700

    First effort on writing an Angular function to call our PHP endpoints. It currently gets a "Missing Username/Password" error but I get the same error when I try to use the endpoints manually so the problem may not be this code.

--- CareWheels/www/js/app.js
+++ CareWheels/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('careWheels', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,34 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller("UserRestController", function($scope, $http, $log, $httpParamSerializerJQLike){
+  $scope.url = 'http://carebank.carewheels.org:8080/userinfo.php';
+  $scope.fetch = function() {
+    $scope.code = null;
+    $scope.response = null;
+    $scope.params = {
+      username:'test', 
+      password:'test123', 
+      usernametofind:'test7'
+    };
+    $http({
+      url:$scope.url, 
+      method:'POST',
+      params: $scope.params
+      /*Looking over the documentation more, I do not believe this service is needed, but I'm leaving it in so
+      we have it if I'm wrong...
+      
+      , params: $httpParamSerializerJQLike({username: 'test', password: 'test123', usernametofind: 'test7'}), 
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }*/
+    }).then(function(response) {
+        $scope.status = response.status;
+        $scope.data = response.data;
+      }, function(response) {
+        $scope.data = response.data || "Request failed";
+        $scope.status = response.status;
+    })
+  };
+});
