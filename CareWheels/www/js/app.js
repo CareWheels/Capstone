bc8e83745a3027dc16323f10fceab164754fba52
Edward Phillips <edward9@pdx.edu>
2016-07-07 11:10:33 -0700

    LastOwnershipTimeTaken and GetLastOwnershipTime
    The get function is not working and I cannot confirm that the updating function is working.

--- CareWheels/www/js/app.js
+++ CareWheels/www/js/app.js
@@ -110,3 +110,80 @@ app.controller("GroupRestController", function($scope, $http, $log, $httpParamSe
     })
   };
 });
+
+//Call to the UpdateLastOwnershipTakenTime endpoint, which logs a user in and updates the LastOwnershipTakenTime field for
+//the member has an alert. This field uses the format XXXX/XX/XX XX:XX:XX. This will report the following error messages upon 
+//failure: "Missing username / password", "Invalid username / password", "Your access is blocked by exceeding invalid login
+//attempts", or a default catch "Error while performing login: errorCode".
+
+// Nothing seems to verify whether or not this is working. I don't know what more can be done.
+app.controller("OwnershipRestController", function($scope, $http, $log, $httpParamSerializerJQLike){
+  $scope.url = 'http://carebank.carewheels.org:8080/updatelastownershiptakentime.php';
+  $scope.fetch = function(userIn, passIn, toUpdate, ownershipTime) {
+    $scope.code = null;
+    $scope.response = null;
+    $http({
+      url:$scope.url, 
+      method:'POST',    //all our custom REST endpoints have been designed to use POST
+      data: $httpParamSerializerJQLike({    //serialize the parameters in the way PHP expects 
+        username:userIn, 
+        password:passIn, 
+        usernametoupdate:toUpdate,
+        lastownershiptakentime:ownershipTime,        
+      }), 
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'   //make Angular use the same content-type header as PHP
+      }
+    }).then(function(response) {    //the old $http success/error methods have been depricated; this is the new format
+        $scope.status = response.status;
+        $scope.data = response.data;
+      }, function(response) {
+        $scope.data = response.data || "Request failed";
+        $scope.status = response.status;
+    })
+  };
+});
+
+
+//This doesn't work at all. I don't know the solution. Looking at the JSON string that returns, there is no instance of 
+//"LastOwnershipTakenTime":XXXX/XX/XX XX:XX:XX anywhere in the string. There are several instances of "id":xxx..xx, so I 
+//tried using id to get something/anything to print out.
+
+//Based on:
+//    var json = '{"count":1, "LastOwnershipTakenTime":"2016/07/06 18:00:00", "count":2}',
+//    obj = JSON.parse(json);
+//    alert(obj.count);
+//    
+//    prints out: 2 ...
+
+//...I figured that obj.id should print out the last instance of id in the string, but nothing printed. I am not sure
+//why. However, since "LastOwnershipTakenTime":XXXX/XX/XX XX:XX:XX does not actually appear in the string, using
+//JSON.parse is probably not the answer. 
+app.controller("GetOwnershipRestController", function($scope, $http, $log, $httpParamSerializerJQLike){
+  $scope.url = 'http://carebank.carewheels.org:8080/groupmemberinfo.php';
+  $scope.fetch = function(userIn, passIn, groupNameIn) {
+    $scope.code = null;
+    $scope.response = null;
+    $http({
+      url:$scope.url, 
+      method:'POST',   //all our custom REST endpoints have been designed to use POST
+      data: $httpParamSerializerJQLike({    //serialize the parameters in the way PHP expects 
+        username:userIn, 
+        password:passIn, 
+        groupInternalName:groupNameIn,        
+      }), 
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'   //make Angular use the same content-type header as PHP
+      }
+    }).then(function(response) {    //the old $http success/error methods have been depricated; this is the new format
+        $scope.status = response.status;
+        $scope.data = response.data;
+        //obj = JSON.parse(response.data);
+        //$scope.data = obj.LastOwnershipTakenTime;      // What I hoped would work
+        //$scope.data = obj.id;                          // Checking to see if anything works (should contain x of last "id":x)
+      }, function(response) {
+        $scope.data = response.data || "Request failed";
+        $scope.status = response.status;
+    })
+  };
+});
