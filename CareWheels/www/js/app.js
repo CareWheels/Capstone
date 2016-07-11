f9a89a68286b89b4407d733c9cadc4552b13e19b
Zack McGinnis <zm3@pdx.edu>
2016-07-10 23:35:04 -0700

    initial commit for SensorDataDownload branch.  Need to add appropriate parameters to the GET and POST requests, respectively

--- CareWheels/www/js/app.js
+++ CareWheels/www/js/app.js
@@ -23,3 +23,77 @@ app.run(function($ionicPlatform) {
   });
 })
 
+/*
+The object (saved to local storage) will contain sen.se token data in the format of:
+{
+  "access_token": "access-token",
+  "refresh_token": "refresh-token",
+  "token_type": "Bearer",
+  "expires_in": 31536000,
+  "scope": "feeds.write profile"
+}
+*/
+
+app.controller("DownloadController", function($scope, $http, $log, $httpParamSerializerJQLike){
+  
+  //called by group member summary status subsystem
+  //will attempt to make GET request to sen.se api /feeds/ endpoint
+  $scope.RequestData = function() {
+  var url = 'https://apis.sen.se/v2/feeds/';  
+  //console.log("test", url);
+  $http({
+    url:url, 
+    method:'GET',
+    data: $httpParamSerializerJQLike({
+      //clientID ?
+      //accessToken: ?      
+    }), 
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authentication': 'Bearer access-token'
+    }
+  }).then(function(response) {
+    $scope.status = response.status;
+    $scope.data = response.data;
+    //TODO:
+    //Save feed data to local storage
+
+    
+    }, function(response) {
+      $scope.data = response.data || "Request failed";
+      $scope.status = response.status;
+      //TODO:
+      //If response fails with 403 "token expired" error,
+      //we call RefreshExpiredToken and pass in
+      //(refreshToken, )
+    })
+  }
+
+    //function which makes POST request to sen.se api
+    //to refresh an expired access token using our refresh token in local storage
+    $scope.RefreshExpiredToken = function() {
+    var refreshUrl = 'https://sen.se/api/v2/oauth2/refresh';
+    $http({
+      url:refreshUrl, 
+      method:'POST',
+      data: $httpParamSerializerJQLike({
+        //refresh_token: JSON.parse(localStorage.getItem(‘member.customField.SenseOAuthRefreshToken’)), 
+        //grant_type: refresh_token
+      }),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then(function(response2) {
+      $scope.status2 = response2.status;
+      $scope.data2 = response2.data;
+      //TODO:
+      //save refreshed accesstoken to local storage
+      //save new refresh token to local storage
+
+    }, function(response2) {
+      $scope.data2= response2.data || "Request failed";
+      $scope.status2 = response2.status;
+    })
+  } //;
+})
+;
