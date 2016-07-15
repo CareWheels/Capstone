d111705ed6584b7688c12b00c6406382b456beb3
Raven <rswartz@pdx.edu>
2016-07-14 17:37:41 -0700

    First pass on notifications component; plugin currently not working so most of it is commented out

--- CareWheels/www/js/app.js
+++ CareWheels/www/js/app.js
@@ -23,3 +23,72 @@ app.run(function($ionicPlatform) {
   });
 })
 
+app.controller("NotificationController", function($scope, $log){
+  //   $scope.Init_Notifs = function() {
+  //   Notif_File = something;
+  //   if(Notif_File==null){
+  //     $scope.notif1 = 10:00:00;
+  //     $scope.notif2 = 14:00:00;
+  //     $scope.notif3 = 19:00:00;
+  //     //make Notif_File
+  //     //save notifs to file
+  //   } else {
+  //     //set notifs to file values
+  //   }
+  //   Create_Notif(notif1, 1);
+  //   Create_Notif(notif2, 2);
+  //   Create_Notif(notif3, 3);
+  // }
+
+  // $scope.Create_Notif = function(time = new Date(),  reminderNum = 0, $log) {
+  //   $log.warn("in Create_Notif ");
+  //   if(reminderNum==0){
+  //     $log.warn("made it to correct state");
+  //     $cordovaLocalNotification.add({
+  //       id: reminderNum,
+  //       message: "There are red alert(s) on your CareWheel!",
+  //       title: "CareWheels",
+  //       autoCancel: true,
+  //       sound: null
+  //     }).then(function() {
+  //       console.log("The notification has been set");
+  //     });
+  //   }
+  //   else if(reminderNum <= 3){
+  //     $cordovaLocalNotification.add({
+  //       id: reminderNum,
+  //       firstAt: time,
+  //       every: "day",
+  //       message: "Reminder " + reminderNum + ": Please check in with your CareWheel!",
+  //       title: "CareWheels",
+  //       autoCancel: true,
+  //       sound: null
+  //     }).then(function() {
+  //       console.log("The notification has been set");
+  //     });      
+  //   } else {
+  //     console.log("Attempted to create notification for id #" + remindNum);
+  //   }
+  //     $log.warn("in else");
+  //   }
+  // }
+
+  // $scope.Delete_Notif = function(id){
+  //   $cordovaLocalNotification.clear(id, function() {
+  //       alert(id + " is done");
+  //   });
+  // }
+  $scope.Create_Notif = function(hours=0, minutes=0, seconds=0, reminderNum=0){
+    if(reminderNum==0){
+      $log.warn("Controller called!" + reminderNum);
+    } else if(reminderNum <4){
+      var time = new Date();
+      time.setHours(hours);
+      time.setMinutes(minutes);
+      time.setSeconds(seconds);
+      $log.warn(time.toUTCString());
+    } else {
+      $log.warn("Attempted to create notification for id #" + reminderNum);
+    }
+  };
+});
