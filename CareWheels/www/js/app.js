be9101d66084440c6f37f7bff41651aaf23edfa2
Raven <rswartz@pdx.edu>
2016-07-14 19:29:57 -0700

    Additional work on Notifications component; current issue is that injecting the plugin doesn't work. I suspect it is because I cannot find ngCordova.

--- CareWheels/www/js/app.js
+++ CareWheels/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('careWheels', ['ionic'])
+var app = angular.module('careWheels', ['ionic', 'ngCordova'])
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,72 +23,57 @@ app.run(function($ionicPlatform) {
   });
 })
 
-app.controller("NotificationController", function($scope, $log){
+app.controller("NotificationController", function($scope, $log, $cordovaLocalNotification){
   //   $scope.Init_Notifs = function() {
   //   Notif_File = something;
   //   if(Notif_File==null){
-  //     $scope.notif1 = 10:00:00;
-  //     $scope.notif2 = 14:00:00;
-  //     $scope.notif3 = 19:00:00;
+  //     Create_Notif(10, 0, 0, 1);
+  //     Create_Notif(14, 0, 0, 2);
+  //     Create_Notif(19, 0, 0, 3);
   //     //make Notif_File
   //     //save notifs to file
   //   } else {
   //     //set notifs to file values
   //   }
-  //   Create_Notif(notif1, 1);
-  //   Create_Notif(notif2, 2);
-  //   Create_Notif(notif3, 3);
   // }
 
-  // $scope.Create_Notif = function(time = new Date(),  reminderNum = 0, $log) {
-  //   $log.warn("in Create_Notif ");
-  //   if(reminderNum==0){
-  //     $log.warn("made it to correct state");
-  //     $cordovaLocalNotification.add({
-  //       id: reminderNum,
-  //       message: "There are red alert(s) on your CareWheel!",
-  //       title: "CareWheels",
-  //       autoCancel: true,
-  //       sound: null
-  //     }).then(function() {
-  //       console.log("The notification has been set");
-  //     });
-  //   }
-  //   else if(reminderNum <= 3){
-  //     $cordovaLocalNotification.add({
-  //       id: reminderNum,
-  //       firstAt: time,
-  //       every: "day",
-  //       message: "Reminder " + reminderNum + ": Please check in with your CareWheel!",
-  //       title: "CareWheels",
-  //       autoCancel: true,
-  //       sound: null
-  //     }).then(function() {
-  //       console.log("The notification has been set");
-  //     });      
-  //   } else {
-  //     console.log("Attempted to create notification for id #" + remindNum);
-  //   }
-  //     $log.warn("in else");
-  //   }
-  // }
-
-  // $scope.Delete_Notif = function(id){
-  //   $cordovaLocalNotification.clear(id, function() {
-  //       alert(id + " is done");
-  //   });
-  // }
   $scope.Create_Notif = function(hours=0, minutes=0, seconds=0, reminderNum=0){
+    $log.warn("Successfully called");
     if(reminderNum==0){
-      $log.warn("Controller called!" + reminderNum);
+      $cordovaLocalNotification.add({
+        id: reminderNum,
+        message: "There are red alert(s) on your CareWheel!",
+        title: "CareWheels",
+        autoCancel: true,
+        sound: null
+      }).then(function() {
+        $log.warn("Alert notification has been set");
+      });
     } else if(reminderNum <4){
       var time = new Date();
       time.setHours(hours);
       time.setMinutes(minutes);
       time.setSeconds(seconds);
-      $log.warn(time.toUTCString());
+
+      $cordovaLocalNotification.add({
+        id: reminderNum,
+        firstAt: time,
+        every: "day",
+        message: "Reminder " + reminderNum + ": Please check in with your CareWheel!",
+        title: "CareWheels",
+        autoCancel: true,
+        sound: null
+      }).then(function() {
+        $log.warn("Notification" + reminderNum + "has been set to " + time.getUTCTime());
+      });    
     } else {
       $log.warn("Attempted to create notification for id #" + reminderNum);
     }
   };
+
+  // $scope.Delete_Notif = function(id){
+  //   $cordovaLocalNotification.clear(id, function() {
+  //       $log.warn(id + " is done");
+  //   });
+  // }
 });
