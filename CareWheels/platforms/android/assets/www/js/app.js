9a4665a4cf31899c113ee618c2d2cf932bfd8e94
Brandon Allen <braallen@pdx.edu>
2016-07-17 22:59:12 -0700

    tested login component on android tablet

--- CareWheels/platforms/android/assets/www/js/app.js
+++ CareWheels/platforms/android/assets/www/js/app.js
@@ -1,29 +1,78 @@
-var app = angular.module('careWheels', ['ionic', 'login.controllers', 'login.services'])
+angular.module('careWheels', [
+  'ionic',
+  'ui.router'
+])
 
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  //contant definition for endpoint base url
+  .constant('BASE_URL', 'https://carebank.carewheels.org:8443')
+
+  .run(function($ionicPlatform) {
+
+//    window.localStorage['loginCredentials'] = null;
+
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  // API factory for making all php endpoints globally accessible.
+  .factory('API', function(BASE_URL) {
+    var api = {
+      userAndGroupInfo:     BASE_URL + '/userandgroupmemberinfo.php',
+      userInfo:             BASE_URL + '/userinfo.php',
+      updateUserReminders:  BASE_URL + '/updateuserreminders.php',
+      groupMemberInfo:      BASE_URL + '/groupmemberinfo.php',
+      updateLastOwnership:  BASE_URL + '/updatelastownershiptakentime.php',
+      dailyTrxHist:         BASE_URL + '/dailytransactionhistory.php'
+    };
+    return api;
+  })
+
+  // GroupInfo factory for global GroupInfo
+  .factory('GroupInfo', function() {
+    return [];
+  })
+
+  // User factory
+  .factory('User', function(GroupInfo, BASE_URL, $http, API, $state, $httpParamSerializerJQLike, $ionicPopup) {
+    var user = {};
+    //window.localStorage['loginCredentials'] = null;
+
+    user.login = function(uname, passwd, rmbr) {
+
+      return $http({
+        url:API.userAndGroupInfo,
+        method: 'POST',
+        data: $httpParamSerializerJQLike({
+            username:uname,
+            password:passwd,
+            usernametofind:uname
+        }),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function(response) {
+        if (rmbr)
+          window.localStorage['loginCredentials'] = angular.toJson({"username":uname, "password":passwd});
+        //store user info
+        //store groupMember info
+        GroupInfo = response.data;
+        $state.go('groupStatus')
+      }, function(response) {
+        //present login failed
+        var alertPopup = $ionicPopup.alert({
+          title: 'Login failed!',
+          template: 'Please check your credentials!'
+        });
+      })
+    };
+
+    return user;
   });
-})
-
-.config(function($stateProvider, $urlRouterProvider) { 
-  $stateProvider.state('login', {
-    url: '/login',
-    templateUrl: 'templates/login.html',
-    controller: 'LoginCtrl'
-  });
-
-  $stateProvider.state('test', {
-    url: '/test',
-    templateUrl: 'templates/test.html',
-  });
-
-  $urlRouterProvider.otherwise('/login');
-});
 
