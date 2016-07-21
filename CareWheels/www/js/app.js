fb0cf64f0517dd18e14bb2b7c6b6d28313cf59cd
Raven <rswartz@pdx.edu>
2016-07-20 17:21:12 -0700

    Fixed buttons so they properly call Create_Notif with "on" param. Also added updated side-menu.

--- CareWheels/www/js/app.js
+++ CareWheels/www/js/app.js
@@ -27,12 +27,13 @@ app.run(function($ionicPlatform) {
 app.controller("NotificationController", function($scope, $log, $cordovaLocalNotification){
   var isAndroid = window.cordova!=undefined;    //checks to see if cordova is available on this platform; platform() erroneously returns 'android' on Chrome Canary so it won't work
   function Time() {this.hours=0; this.minutes=0; this.seconds=0; this.on=true;};
-  window.localStorage['Reminders'] = null;    //Turning this on simulates starting from fresh storage every time controller is called by view change
+  //window.localStorage['Reminders'] = null;    //Turning this on simulates starting from fresh storage every time controller is called by view change
   $scope.data = angular.fromJson(window.localStorage['Reminders']);   //needs to be called outside the functions so it persists for all of them
 
   //To be called during app startup after login; retrieves saved alert times (if they exist) or creates default alerts (if they don't) 
   //and calls Create_Notif for each of them
   $scope.Init_Notifs = function() {
+    //$scope.data = angular.fromJson(window.localStorage['Reminders']);
     if($scope.data==null){   //have notifications been initialized before?
       $scope.data = [];    //data param needs to be initialized before indices can be added
       $scope.data[0] = new Time();
@@ -51,7 +52,8 @@ app.controller("NotificationController", function($scope, $log, $cordovaLocalNot
 
   //Schedules a local notification and, if it is a reminder, saves a record of it to local storage. reminderNum must be <4
   //or it will log an error and schedule no notifications.
-  $scope.Create_Notif = function(hours=0, minutes=0, seconds=0, isOn=true, reminderNum=0){
+  $scope.Create_Notif = function(hours, minutes, seconds, isOn, reminderNum){
+    //$scope.data = angular.fromJson(window.localStorage['Reminders']);
     if(reminderNum==0){   //is notif a red alert?
       if(isAndroid){
         $cordovaLocalNotification.schedule({    //omitting 'at' and 'every' params means it occurs once, immediately
@@ -63,8 +65,8 @@ app.controller("NotificationController", function($scope, $log, $cordovaLocalNot
           $log.log("Alert notification has been set");
         });        
       } else $log.warn("Plugin disabled");
-    } else if(reminderNum <4){    //is notif a user reminder?
-      var time = new Date();    //defaults to current date/time
+    } if(reminderNum>0 && reminderNum <4){    //is notif a user reminder?
+      var time = new Date($scope.data[0].hours + ":" + $scope.data[0].minutes);    //defaults to current date/time
       time.setHours(hours);     //update 
       $scope.data[reminderNum-1].hours = hours;
       time.setMinutes(minutes);
@@ -73,7 +75,6 @@ app.controller("NotificationController", function($scope, $log, $cordovaLocalNot
       $scope.data[reminderNum-1].seconds = seconds;
       $scope.data[reminderNum-1].on = isOn;
       window.localStorage['Reminders'] = angular.toJson($scope.data);   //save $scope.data so new reminder is stored
-
       if(isAndroid){
         $cordovaLocalNotification.schedule({
           id: reminderNum,
@@ -86,12 +87,13 @@ app.controller("NotificationController", function($scope, $log, $cordovaLocalNot
           $log.log("Notification" + reminderNum + "has been scheduled for " + time.getUTCTime() + ", daily");
         });    
       } else $log.warn("Plugin disabled"); 
-    } else $log.warn("Incorrect attempt to create notification for id #" + reminderNum);
+    } else if(reminderNum >=4) $log.warn("Incorrect attempt to create notification for id #" + reminderNum);
   };
 
   //Unschedules a local notification; clears its index if it is a user reminder (id 1-3). If id is invalid clear() will not
   //throw errors. Delete_Notif(0) is technically valid but Red Alerts are one-time and instant so unscheduling them is unnecessary.
   $scope.Delete_Notif = function(id){   //NOTE: id corresponds to $scope.data array indices so it is off by one
+    //$scope.data = angular.fromJson(window.localStorage['Reminders']);
     if(isAndroid){
       $cordovaLocalNotification.clear(id, function() {
         $log.log(id + " is cleared");
@@ -105,6 +107,7 @@ app.controller("NotificationController", function($scope, $log, $cordovaLocalNot
 
   //Unschedules a local notification as per Delete_Notif but does NOT clear storage or data index; to be used by User Reminder's Toggle()
   $scope.Toggle_Off_Notif = function(id){
+    $scope.data = angular.fromJson(window.localStorage['Reminders']);
     if(id==1||id==2||id==3){
       $scope.data[id-1].on = false;
       window.localStorage['Reminders'] = angular.toJson($scope.data);   //and save $scope.data so toggle is remembered
@@ -118,7 +121,7 @@ app.controller("NotificationController", function($scope, $log, $cordovaLocalNot
 
   //prints the in-memory and scheduled status of Reminders, for testing purposes
   $scope.Notifs_Status = function(){
-    $scope.data = angular.fromJson(window.localStorage['Reminders']);
+    //$scope.data = angular.fromJson(window.localStorage['Reminders']);
     alert("In memory: \nReminder 1= (" +$scope.data[0].on +") "+ $scope.data[0].hours + ":" + $scope.data[0].minutes + ":" + $scope.data[0].seconds +
       "\nReminder 2= (" +$scope.data[0].on +") "+ $scope.data[1].hours + ":" + $scope.data[1].minutes + ":" + $scope.data[1].seconds +
       "\nReminder 3= (" +$scope.data[0].on +") "+ $scope.data[2].hours + ":" + $scope.data[2].minutes + ":" + $scope.data[2].seconds);
