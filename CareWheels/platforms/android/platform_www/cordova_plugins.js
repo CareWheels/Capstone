acd9042a13f357e4e18bd9e6ed78e353e31097bb
Zack McGinnis <zm3@pdx.edu>
2016-07-25 21:09:00 -0700

    fixing ionic build issues...

--- CareWheels/platforms/android/platform_www/cordova_plugins.js
+++ CareWheels/platforms/android/platform_www/cordova_plugins.js
@@ -2,6 +2,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "cordova-plugin-whitelist": "1.2.3-dev"
+};
 // BOTTOM OF METADATA
 });
