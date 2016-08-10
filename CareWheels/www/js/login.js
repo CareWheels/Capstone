e898f1da7048b7494d732c11de1335bf5a377d24
c-asakawa <chris@asakawa.me>
2016-08-09 20:35:00 -0700

    added login timeout

--- CareWheels/www/js/login.js
+++ CareWheels/www/js/login.js
@@ -6,9 +6,12 @@ angular.module('careWheels')
 
   .controller('loginController', function($scope, $controller, User, $state, $ionicLoading, GroupInfo, $interval){
 
-    var DOWNLOAD_INTERVAL = 1000 * 60 * 5; // constant interval for download
+    var DOWNLOAD_INTERVAL = 1000 * 60 * 5; // constant interval for download, 5mins
     var dataDownload = $scope.$new();
     var dataAnalysis = $scope.$new();
+    var loginTimeout = false;
+    var loginIntervalSteps = 0;
+
     var credentials = angular.fromJson(window.localStorage['loginCredentials']);
 
     $controller('DownloadCtrl', {$scope : dataDownload});
@@ -16,15 +19,9 @@ angular.module('careWheels')
 
     $scope.rememberMe = false;
     $scope.logoImage = 'img/CareWheelsLogo.png';
-    $scope.loginTimeout = false;
-    $scope.loginAttempts = 0;
 
     $scope.login = function(uname, passwd, rmbr) {
       User.login(uname, passwd, rmbr).then(function(response) {
-
-        setTimeout(function(){
-          $scope.loginTimeout = true;
-        }, 1000 * 3);
 
         if (User.credentials()) {
           $ionicLoading.show({      //pull up loading overlay so user knows App hasn't frozen
@@ -35,6 +32,13 @@ angular.module('careWheels')
 
           // store the interval promise in this variable
           var intervalPromise = $interval( function(){
+
+            if (loginIntervalSteps > 120) // 120 * 500 = 1min timeout
+              loginTimeout = true;
+
+            // keep track of how many times we step through the interval
+            loginIntervalSteps++;
+
             var info = GroupInfo.groupInfo();
             try {
               dataAnalysis.AnalyzeData();
@@ -43,13 +47,13 @@ angular.module('careWheels')
               console.log('waiting for download to finish')
             }
             // sweet we got data, lets break out of this interval
-            if (info[4].analysisData !== null){
+            if (info[4].analysisData !== null || loginTimeout){
               $interval.cancel(intervalPromise);
               $ionicLoading.hide();   //make sure to hide loading screen
               scheduleDownload();
               $state.go('app.groupStatus');
             }
-          }, 100 );
+          }, 500 );
         }
       });
     };
@@ -70,6 +74,7 @@ angular.module('careWheels')
         }, 1000 * 60 * 2); // give the download two mins before analyze
       }, DOWNLOAD_INTERVAL ); // 5 min interval
     }
+
 
     if (credentials)
       $scope.login(credentials.username, credentials.password, true);
