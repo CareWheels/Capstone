25b2e0472cb64c38beb0c3ecfe63487101096f68
c-asakawa <chris@asakawa.me>
2016-08-09 19:13:28 -0700

    cleaned up

--- CareWheels/www/js/login.js
+++ CareWheels/www/js/login.js
@@ -7,10 +7,8 @@ angular.module('careWheels')
   .controller('loginController', function($scope, $controller, User, $state, $ionicLoading, GroupInfo, $interval){
 
     var DOWNLOAD_INTERVAL = 1000 * 60 * 5; // constant interval for download
-
     var dataDownload = $scope.$new();
     var dataAnalysis = $scope.$new();
-
     var credentials = angular.fromJson(window.localStorage['loginCredentials']);
 
     $controller('DownloadCtrl', {$scope : dataDownload});
@@ -36,22 +34,23 @@ angular.module('careWheels')
           dataDownload.DownloadData();
 
           // store the interval promise in this variable
-          var intervalPromise;
-
-          // stops the interval
-          $scope.stopInterval = function() { $interval.cancel(intervalPromise); };
-          // starts the interval
-          $scope.startInterval = function() {
-            // store the interval promise
-            intervalPromise = $interval( analyzeData, 100 );
-          };
-
-          // starting the interval by default
-          $scope.startInterval();
-
-
+          var intervalPromise = $interval( function(){
+            var info = GroupInfo.groupInfo();
+            try {
+              dataAnalysis.AnalyzeData();
+            }
+            catch (Exception){
+              console.log('waiting for download to finish')
+            }
+            // sweet we got data, lets break out of this interval
+            if (info[4].analysisData !== null){
+              $interval.cancel(intervalPromise);
+              $ionicLoading.hide();   //make sure to hide loading screen
+              scheduleDownload();
+              $state.go('app.groupStatus');
+            }
+          }, 100 );
         }
-
       });
     };
 
@@ -72,48 +71,7 @@ angular.module('careWheels')
       }, DOWNLOAD_INTERVAL ); // 5 min interval
     }
 
-
-    function analyzeData() {
-      var info = GroupInfo.groupInfo();
-      try {
-        dataAnalysis.AnalyzeData();
-      }
-      catch (Exception){
-        console.log('waiting for download to finish')
-      }
-      // sweet we got data, lets break out of this interval
-      if (info[4].analysisData !== null){
-        $state.go('app.groupStatus');
-        $scope.stopInterval();
-        $ionicLoading.hide();   //make sure to hide loading screen
-        scheduleDownload();
-      }
-    }
-
-
     if (credentials)
       $scope.login(credentials.username, credentials.password, true);
-
-    // $ionicModal.fromTemplateUrl('views/login.html', {
-    //   scope: $scope
-    // }).then(function(modal) {
-    //   $scope.modal = modal;
-    // });
-
-
-
-
-
-    // $scope.doLogin = function(username, password, rememberMe) {
-    //   User.login(username, password, rememberMe, $scope.done);
-    // }
-
-    // $scope.login = function() {
-    //   $scope.modal.show();
-    // };
-
-    // $scope.done = function() {
-    //   $scope.modal.hide();
-    // }
 
   });
