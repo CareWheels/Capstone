6102ce44d08dad5e860fb5c458f118f7c124a783
c-asakawa <chris@asakawa.me>
2016-08-09 17:25:02 -0700

    fixed interval

--- CareWheels/www/js/login.js
+++ CareWheels/www/js/login.js
@@ -4,47 +4,51 @@
  */
 angular.module('careWheels')
 
-  .controller('loginController', function($scope, $controller, User, $state, $ionicLoading, GroupInfo){
+  .controller('loginController', function($scope, $controller, User, $state, $ionicLoading, GroupInfo, $interval){
 
     var DOWNLOAD_INTERVAL = 1000 * 60 * 5; // constant interval for download
 
     var dataDownload = $scope.$new();
     var dataAnalysis = $scope.$new();
+
+    var credentials = angular.fromJson(window.localStorage['loginCredentials']);
+
     $controller('DownloadCtrl', {$scope : dataDownload});
     $controller('AnalysisCtrl', {$scope : dataAnalysis});
 
     $scope.rememberMe = false;
     $scope.logoImage = 'img/CareWheelsLogo.png';
+    $scope.loginTimeout = false;
+    $scope.loginAttempts = 0;
 
     $scope.login = function(uname, passwd, rmbr) {
       User.login(uname, passwd, rmbr).then(function(response) {
 
+        setTimeout(function(){
+          $scope.loginTimeout = true;
+        }, 1000 * 3);
+
         if (User.credentials()) {
           $ionicLoading.show({      //pull up loading overlay so user knows App hasn't frozen
-            template: '<ion-spinner></ion-spinner>'+
-                  '<p>Contacting Server...</p>'
+            template: '<ion-spinner></ion-spinner>' + '<p>Contacting Server...</p>'
           });
           // do data download here
           dataDownload.DownloadData();
 
-          var trigger = setInterval(function(){
+          // store the interval promise in this variable
+          var intervalPromise;
 
-            var info = GroupInfo.groupInfo();
-            try {
-              dataAnalysis.AnalyzeData();
-            }
-            catch (Exception){
-              console.log('waiting for download to finish')
-            }
+          // stops the interval
+          $scope.stopInterval = function() { $interval.cancel(intervalPromise); };
+          // starts the interval
+          $scope.startInterval = function() {
+            // store the interval promise
+            intervalPromise = $interval( analyzeData, 100 );
+          };
 
-            // sweet we got data, lets break out of this interval
-            if (info[4].analysisData !== null){
-              clearInterval(trigger);
-              $state.go('app.groupStatus');
-              $ionicLoading.hide();   //make sure to hide loading screen
-              scheduleDownload();
-            }
-          }, 500); //checks every half second
+          // starting the interval by default
+          $scope.startInterval();
+
 
         }
 
@@ -60,19 +64,32 @@ angular.module('careWheels')
      *      3. analyze data
      * */
     function scheduleDownload(){
-      setInterval(function(){
+      $interval(function(){
         dataDownload.DownloadData();
-
         setTimeout(function(){
           dataAnalysis.AnalyzeData();
         }, 1000 * 60 * 2); // give the download two mins before analyze
-
       }, DOWNLOAD_INTERVAL ); // 5 min interval
-
     }
 
 
-    var credentials = angular.fromJson(window.localStorage['loginCredentials']);
+    function analyzeData() {
+      var info = GroupInfo.groupInfo();
+      try {
+        dataAnalysis.AnalyzeData();
+      }
+      catch (Exception){
+        console.log('waiting for download to finish')
+      }
+      // sweet we got data, lets break out of this interval
+      if (info[4].analysisData !== null){
+        $state.go('app.groupStatus');
+        $scope.stopInterval();
+        $ionicLoading.hide();   //make sure to hide loading screen
+        scheduleDownload();
+      }
+    }
+
 
     if (credentials)
       $scope.login(credentials.username, credentials.password, true);
