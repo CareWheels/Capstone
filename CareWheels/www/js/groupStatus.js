4ca627299a166dce73cf94975a75e9deed79c631
c-asakawa <chris@asakawa.me>
2016-08-16 12:42:32 -0700

    implemented dynamic way to get user who logged in

--- CareWheels/www/js/groupStatus.js
+++ CareWheels/www/js/groupStatus.js
@@ -3,25 +3,27 @@
  *
  */
 angular.module('careWheels').controller('groupStatusController',
-  function ($scope, $interval, $state, $ionicPopup, GroupInfo, PaymentService) {
+  function ($scope, $interval, $state, $ionicPopup, GroupInfo, User, PaymentService) {
 
-    /* TODO find a better solution */
     // the groupInfo object is not available immediately, spin until available
     var initGroupInfo = setInterval(function(){
       var groupArray = GroupInfo.groupInfo();
       //console.log(groupArray);
       if ( groupArray[0] != null ){
         clearInterval(initGroupInfo);
-        $scope.group[0].balance = trimZeros(groupArray[0].balance);
-        for (var i = 0; i < groupArray.length; i++){
-          $scope.group[i].image = groupArray[i].photoUrl;
-          $scope.group[i].username = groupArray[i].username;
-          $scope.group[i].name = groupArray[i].name;
+        // app on load
+        if ($scope.group[0].selfUserIndex == -1 || $scope.group[0].selfUserIndex == null){
+          getLoggedInUser(groupArray);
+          setGroupArray(groupArray);
+        }
+        // else state change
+        else{
+          setGroupArray(groupArray);
         }
       }
     }, 50);
 
-    $interval(function(){
+/*    $interval(function(){
       var groupArray = GroupInfo.groupInfo();
       for (var i = 1; i < groupArray.length; i ++){
         try{
@@ -38,7 +40,7 @@ angular.module('careWheels').controller('groupStatusController',
           $scope.checkGroupHealth();
         }
       }
-    }, 1000);
+    }, 1000);*/
 
     /** automatically go through each user square, and
      *  find each 'red' alert, and fade that element in
@@ -68,7 +70,8 @@ angular.module('careWheels').controller('groupStatusController',
         balance: '0.0',
         image: '',
         userSelected: '',
-        displayedError: false
+        displayedError: false,
+        selfUserIndex: -1
 
       },
       { // top left @ index 1
@@ -110,7 +113,58 @@ angular.module('careWheels').controller('groupStatusController',
     $scope.clickCareBank    = function () {};
 
     // console.log("Calling memberSummary Payment:");
-    // PaymentService.memberSummary(1.0);    
+    // PaymentService.memberSummary(1.0);
+
+    // lets figure out which user logged in at this point
+    function getLoggedInUser(groupInfo){
+      var user = User.credentials();
+      // error unable to load user object;
+      if (user == null)
+        $scope.group[0].selfUserIndex = 0;
+      // loop through the groupInfo array to find the user who
+      // logged in.
+      for (var i=0; i < groupInfo.length; i++){
+        if (user.username == $scope.group[i].username)
+          $scope.group[0].selfUserIndex = i; // gotcha!
+      }
+      $scope.group[0].selfUserIndex = 0; //error, user not found
+    }
+
+
+    // now lets set the scope variables for all the page views.
+    function setGroupArray(groupArray) {
+      var loggedInUserIndex = $scope.group[0].selfUserIndex;
+
+      // first lets set the group up, at index 1-4
+      for (var i=1; i < 5; i++){
+        if (i != $scope.group[0].selfUserIndex){
+          $scope.group[i].image = groupArray[i].photoUrl;
+          $scope.group[i].username = groupArray[i].username;
+          $scope.group[i].name = groupArray[i].name;
+
+          try{
+            var fridgeAlert = groupArray[i].analysisData.fridgeAlertLevel;
+            var medsAlert = groupArray[i].analysisData.medsAlertLevel;
+            $scope.group[i].status = $scope.getAlertColor(fridgeAlert, medsAlert);
+          }
+          catch(Exception) {
+            $scope.group[i].status = 'grey';
+            $scope.group[i].error = true;
+          }
+          // on the last element of the loop, now check health
+          if (i == groupArray.length - 1){
+            $scope.checkGroupHealth();
+          }
+        }
+      }
+      // sweet, next lets set the data for the user that logged in,
+      // this is reserved at index zero.
+      $scope.group[0].image = groupArray[loggedInUserIndex].photoUrl;
+      $scope.group[0].username = groupArray[loggedInUserIndex].username;
+      $scope.group[0].name = groupArray[loggedInUserIndex].name;
+      $scope.group[0].balance = trimZeros(groupArray[0].balance);
+    }
+
 
 
     //removes insignificant zeros
