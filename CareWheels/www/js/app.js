8cbd4cdb45c854937a45b1d1c437b6bccf634ce5
Raven <rswartz@pdx.edu>
2016-08-19 12:33:19 -0700

    Version number change, primarily for meeting clarity.

--- CareWheels/www/js/app.js
+++ CareWheels/www/js/app.js
@@ -19,7 +19,7 @@ angular.module('careWheels', [
 .constant('BASE_URL', 'https://carewheels.cecs.pdx.edu:8443')
 
 // change the version number here
-.constant('VERSION_NUMBER', '0.03')
+.constant('VERSION_NUMBER', '0.04')
 
 .run(function ($rootScope, $ionicPlatform, $ionicHistory, $state, $window, User) {
 
