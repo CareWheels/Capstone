42c63e5c893d70bd423b4f8589c07a3b30aba06b
c-asakawa <chris@asakawa.me>
2016-08-22 20:31:02 -0700

    fixed vacation mode bug

--- CareWheels/www/js/groupStatus.js
+++ CareWheels/www/js/groupStatus.js
@@ -5,6 +5,7 @@
 angular.module('careWheels').controller('groupStatusController',
   function ($scope, $interval, $state, $ionicPopup, GroupInfo, User, PaymentService) {
 
+
     // the groupInfo object is not available immediately, spin until available
     // toDo: remove this once the callbacks for downland and analysis are set up
     var initGroupInfo = setInterval(function () {
@@ -13,15 +14,28 @@ angular.module('careWheels').controller('groupStatusController',
         clearInterval(initGroupInfo);
         getLoggedInUser(groupArray);
         setGroupArray(groupArray);
+        checkVacationMode();
       }
     }, 50);
 
+
+    runOnStateChange();
     /**
-     * credit hte user for viewing the user summary
-     * this function is invoked with each state
-     * change to this view.
+     *  this function is invoked with each state chang to this view.
      */
-    creditPageHit();
+    function runOnStateChange() {
+      console.log('crediting user for group summary view.');
+      PaymentService.memberSummary(0.1);
+      //checkVacationMode();
+    }
+
+    function checkVacationMode() {
+      if (User.getVacationValue())
+        $('#vacationMode').fadeIn(0);
+      else
+        $('#vacationMode').fadeOut(0);
+    }
+
 
     /** automatically go through each user square, and
      *  find each 'red' alert, and fade that element in
@@ -107,14 +121,14 @@ angular.module('careWheels').controller('groupStatusController',
     function getLoggedInUser(groupInfo) {
       var user = User.credentials();
       // error unable to load user object;
-      if (user == null){
+      if (user == null) {
         $scope.group[0].selfUserIndex = -1;
       }
 
       // loop through the groupInfo array to find the user who
       // logged in.
       for (var i = 0; i < groupInfo.length; i++) {
-        if (user.username == groupInfo[i].username){
+        if (user.username == groupInfo[i].username) {
           $scope.group[0].selfUserIndex = i; // gotcha!
           return true;
         }
@@ -138,14 +152,14 @@ angular.module('careWheels').controller('groupStatusController',
       currentUser++; // = 1 at this point
 
       // put everyone else into the array
-      for (var i = 0; i < 5; i++){
+      for (var i = 0; i < 5; i++) {
         // skip the user who logged in
-        if(i != $scope.group[0].selfUserIndex){
+        if (i != $scope.group[0].selfUserIndex) {
           $scope.group[currentUser].image = groupArray[i].photoUrl;
           $scope.group[currentUser].username = groupArray[i].username;
           $scope.group[currentUser].name = groupArray[i].name;
 
-          try{
+          try {
             fridgeAlert = groupArray[i].analysisData.fridgeAlertLevel;
             medsAlert = groupArray[i].analysisData.medsAlertLevel;
             $scope.group[currentUser].status = $scope.getAlertColor(fridgeAlert, medsAlert, i);
@@ -158,19 +172,12 @@ angular.module('careWheels').controller('groupStatusController',
         }
         // on the last element of the loop, now check health
         if (i == 4) {
-          if(!GroupInfo.getSensorError())
+          if (!GroupInfo.getSensorError())
             $scope.checkGroupHealth();
         }
 
       }
     }
-
-    // this runs everytime that the user changes state to this view.
-    function creditPageHit() {
-      console.log('crediting user for group summary view.');
-      PaymentService.memberSummary(0.1);
-    }
-
 
     //removes insignificant zeros
     function trimZeros(input) {
@@ -243,12 +250,12 @@ angular.module('careWheels').controller('groupStatusController',
         // on the last element now
         if (i == $scope.group.length - 1) {
           // no errors? then return
-          if (errorCount == 0){
+          if (errorCount == 0) {
             GroupInfo.setSensorError(false);
             return true;
           }
           // error found! set the error variable
-          if(errorCount > 0)
+          if (errorCount > 0)
             GroupInfo.setSensorError(true);
 
           //lets craft up a string to be displayed
