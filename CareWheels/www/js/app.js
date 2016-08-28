9ffd827ea7614a1dcae1f4098844ebd106b1e112
Raven <rswartz@pdx.edu>
2016-08-27 17:50:33 -0700

    Changing version number to 1.00, to signify that the capstone team has finished working on it.

--- CareWheels/www/js/app.js
+++ CareWheels/www/js/app.js
@@ -19,7 +19,7 @@ angular.module('careWheels', [
 .constant('BASE_URL', 'https://carewheels.cecs.pdx.edu:8443')
 
 // change the version number here
-.constant('VERSION_NUMBER', '0.10')
+.constant('VERSION_NUMBER', '1.00')
 
 .run(function ($rootScope, $ionicPlatform, $ionicHistory, $state, $window, User) {
 
